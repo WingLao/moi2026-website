b66bcafae4afa2d9a5964b80996a2c4182fa18dc
WingLao <99243673+WingLao@users.noreply.github.com>
2026-03-29 21:00:22 +0800

    Exit process after logging warnings

--- scripts/import-problems.ts
+++ scripts/import-problems.ts
@@ -3,7 +3,7 @@ import { importProblems } from '../src/lib/problem-import';
 const root = process.argv[2] || process.env.JUDGE_DATA_ROOT;
 
 importProblems(root)
-  .then((warnings) => console.log(JSON.stringify({ warnings }, null, 2)))
+    .then((warnings) => { console.log(JSON.stringify({ warnings }, null, 2)); process.exit(0); })
   .catch((error) => {
     console.error(error);
     process.exit(1);
